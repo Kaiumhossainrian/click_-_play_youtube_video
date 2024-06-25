1b3943ea8c962c33898c3473b63ffa3699c6257f
Md Kaium Hossain Molla <rian.nits90@gmail.com>
2024-06-25 16:43:48 +0600

    ll

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,58 @@
     }
   });
 
+
+
+  // Load the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Create a global YouTube player variable.
+  window.player = null;
+
+  // This function creates an <iframe> (and YouTube player)
+  // after the API code downloads.
+  window.onYouTubeIframeAPIReady = function () {
+      window.player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          playerVars: {
+              'playsinline': 1
+          },
+          events: {
+              'onReady': window.onPlayerReady,
+              'onStateChange': window.onPlayerStateChange
+          }
+      });
+  };
+
+  // The API will call this function when the video player is ready.
+  window.onPlayerReady = function (event) {
+      document.getElementById("play").addEventListener("click", function() {
+          event.target.playVideo();
+      });
+  };
+
+  // The API calls this function when the player's state changes.
+  window.onPlayerStateChange = function (event) {
+      if (event.data == YT.PlayerState.PLAYING && !window.done) {
+          window.done = true;
+      }
+  };
+
+  window.stopVideo = function () {
+      if (window.player) {
+          window.player.stopVideo();
+      }
+  };
+
+  // Initialize the done variable
+  window.done = false;
+
+
   // Player
   var toggleIcon = document.querySelectorAll('.cover')
 
