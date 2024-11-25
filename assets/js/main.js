24fc3ba187655cc23270f0809506d92bb54b1474
Md Kaium Hossain Molla <rian.nits90@gmail.com>
2024-11-25 12:16:36 +0600

    update data

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,59 +22,66 @@
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+  var videoElement = document.getElementById('player');
+  var videoId = videoElement.getAttribute('data-video-id');
+
+  if (!videoId) {
+    console.error('No video ID provided in the HTML element.');
+    return; // Exit the script to avoid errors
+  }
+
   // Create a global YouTube player variable.
   window.player = null;
 
   // This function creates an <iframe> (and YouTube player)
   // after the API code downloads.
   window.onYouTubeIframeAPIReady = function () {
-      window.player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'M7lc1UVf-VE',
-          playerVars: {
-              'playsinline': 1
-          },
-          events: {
-              'onReady': window.onPlayerReady,
-              'onStateChange': window.onPlayerStateChange
-          }
-      });
+    window.player = new YT.Player('player', {
+      videoId: videoId, // Use the dynamically retrieved videoId
+      playerVars: {
+        'playsinline': 1
+      },
+      events: {
+        'onReady': window.onPlayerReady,
+        'onStateChange': window.onPlayerStateChange
+      }
+    });
   };
 
   // The API will call this function when the video player is ready.
   window.onPlayerReady = function (event) {
-      document.getElementById("play").addEventListener("click", function() {
-          event.target.playVideo();
-      });
+    document.getElementById("play").addEventListener("click", function () {
+      event.target.playVideo();
+    });
   };
 
   // The API calls this function when the player's state changes.
   window.onPlayerStateChange = function (event) {
-      if (event.data == YT.PlayerState.PLAYING && !window.done) {
-          window.done = true;
-      }
+    if (event.data == YT.PlayerState.PLAYING && !window.done) {
+      window.done = true;
+    }
   };
 
   window.stopVideo = function () {
-      if (window.player) {
-          window.player.stopVideo();
-      }
+    if (window.player) {
+      window.player.stopVideo();
+    }
   };
 
   // Initialize the done variable
   window.done = false;
 
 
-  // Player
+
+  // Player Button
   var toggleIcon = document.querySelectorAll('.cover')
 
-  toggleIcon.forEach((element)=>{
-      element.addEventListener('click', ()=>{
-          document.querySelectorAll('.play-btn-wrap').forEach((el)=>{
-              el.classList.add('hide')
-          })
+  toggleIcon.forEach((element) => {
+    element.addEventListener('click', () => {
+      document.querySelectorAll('.play-btn-wrap').forEach((el) => {
+        el.classList.add('hide')
       })
+    })
   })
 
 
